9ed1be83b34ecfe848390e0784f094858b792cef
Gabriel Willian <gabrielgws@DESKTOP-73ONRTC.localdomain>
2022-03-14 09:51:06 -0300

    att SEO

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -11,6 +11,12 @@ export default class MyDocument extends Document {
           <link rel="shortcut icon" href="/favicon.png" type="image/png" />
     
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet"></link>
+
+          <meta name="keywords" content="advogado, advogados, adevogado, adevogados, Mitrione Advogado, Luckezzi Advogado, Mitrione & Luckezzi, Belo Horizonte (MG), leito hospitalar, Leito urgente, Nova lima" />
+          <meta name="description" content="Assessoria jurídica especializada em demandas que envolvem a necessidade de Leito Hospitalar com urgência." />
+          <meta name="author" content="Mitrione & Luckezzi - Advogados" />
+          <meta name="creator" content="@gabrielgws" />
+          <meta name="robots" content="index, follow" />
         </Head>
 
         <body>
